ef919c8ea0d0f40b21113e15f400c3fbe979cf66
Benjamin Winterberg <winterbe@googlemail.com>
2014-03-16 18:26:47 +0100

    first commit

---
+++ src/com/winterbe/java8/Lambda1.java
@@ -0,0 +1,34 @@
+package com.winterbe.java8;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author Benjamin Winterberg
+ */
+public class Lambda1 {
+
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("peter", "anna", "mike", "xenia");
+
+        Collections.sort(names, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return b.compareTo(a);
+            }
+        });
+
+        Collections.sort(names, (String a, String b) -> {
+            return b.compareTo(a);
+        });
+
+        Collections.sort(names, (String a, String b) -> b.compareTo(a));
+
+        Collections.sort(names, (a, b) -> b.compareTo(a));
+
+        System.out.println(names);
+    }
+
+}
---
+++ src/com/winterbe/java8/Person.java
@@ -0,0 +1,16 @@
+package com.winterbe.java8;
+
+/**
+* @author Benjamin Winterberg
+*/
+class Person {
+    String firstName;
+    String lastName;
+
+    Person() {}
+
+    Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
