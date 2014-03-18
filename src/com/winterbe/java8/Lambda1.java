3693f6b23064b4a4b0e8277081e9297182f342d6
Benjamin Winterberg <winterbe@googlemail.com>
2014-03-18 22:48:27 +0100

    remove obsolete assert

--- src/com/winterbe/java8/Lambda1.java
+++ src/com/winterbe/java8/Lambda1.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -45,8 +44,6 @@ public class Lambda1 {
         Optional.ofNullable(names3).ifPresent(list -> list.sort(Comparator.naturalOrder()));
 
         System.out.println(names3);
-
-        Objects.nonNull(names3);
     }
 
 }
