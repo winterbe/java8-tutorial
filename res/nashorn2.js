3d559e52ad59d9fd392b0719e1869dc372236998
Benjamin Winterberg <winterbe@googlemail.com>
2014-03-31 18:08:45 +0200

    pass js objects to java and work with properties and members on java side

--- res/nashorn2.js
+++ res/nashorn2.js
@@ -1,3 +1,29 @@
 var Nashorn2 = Java.type('com.winterbe.java8.Nashorn2');
 var result = Nashorn2.fun('John Doe');
 print('\n' + result);
+
+Nashorn2.fun2(new Date());
+Nashorn2.fun2(new RegExp());
+Nashorn2.fun2({foo: 'bar'});
+
+
+print('passing object hash:');
+Nashorn2.fun3({
+    foo: 'bar',
+    bar: 'foo'
+});
+
+
+print('passing custom person object:');
+
+function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.getFullName = function() {
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+var person1 = new Person("Peter", "Parker");
+Nashorn2.fun3(person1);
+Nashorn2.fun4(person1);
