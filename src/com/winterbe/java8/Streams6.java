0e21011ec92d791af52539db8aaf863f450a3ed8
Benjamin Winterberg <winterbe@googlemail.com>
2014-07-17 17:47:05 +0200

    Add more stream examples

---
+++ src/com/winterbe/java8/Streams6.java
@@ -0,0 +1,33 @@
+package com.winterbe.java8;
+
+import java.util.Optional;
+
+/**
+ * @author Benjamin Winterberg
+ */
+public class Streams6 {
+
+    static class Outer {
+        Nested nested;
+    }
+
+    static class Nested {
+        Inner inner;
+    }
+
+    static class Inner {
+        String foo;
+    }
+
+    public static void main(String[] args) {
+        test1();
+    }
+
+    private static void test1() {
+        Optional.of(new Outer())
+            .flatMap(o -> Optional.ofNullable(o.nested))
+            .flatMap(n -> Optional.ofNullable(n.inner))
+            .flatMap(i -> Optional.ofNullable(i.foo))
+            .ifPresent(System.out::print);
+    }
+}
