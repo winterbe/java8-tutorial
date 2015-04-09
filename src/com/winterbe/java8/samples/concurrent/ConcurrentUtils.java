b81681c36756124cc74db3ee6a7e3e841d247d75
Benjamin Winterberg <winterbe@googlemail.com>
2015-04-09 07:41:55 +0200

    Test synchronized

---
+++ src/com/winterbe/java8/samples/concurrent/ConcurrentUtils.java
@@ -0,0 +1,27 @@
+package com.winterbe.java8.samples.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Benjamin Winterberg
+ */
+public class ConcurrentUtils {
+
+    public static void stop(ExecutorService executor) {
+        try {
+            executor.shutdown();
+            executor.awaitTermination(5, TimeUnit.SECONDS);
+        }
+        catch (InterruptedException e) {
+            System.err.println("termination interrupted");
+        }
+        finally {
+            if (!executor.isTerminated()) {
+                System.err.println("killing non-finished tasks");
+            }
+            executor.shutdownNow();
+        }
+    }
+
+}
---
+++ src/com/winterbe/java8/samples/concurrent/Synchronized1.java
@@ -0,0 +1,54 @@
+package com.winterbe.java8.samples.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
+
+/**
+ * @author Benjamin Winterberg
+ */
+public class Synchronized1 {
+
+    private static final int NUM_INCREMENTS = 10000;
+
+    private static int count = 1;
+
+    public static void main(String[] args) {
+        testSyncIncrement();
+        testNonSyncIncrement();
+    }
+
+    private static void testSyncIncrement() {
+        count = 1;
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        IntStream.range(0, NUM_INCREMENTS)
+                .forEach(i -> executor.submit(Synchronized1::incrementSync));
+
+        ConcurrentUtils.stop(executor);
+
+        System.out.println("   Sync: " + count);
+    }
+
+    private static void testNonSyncIncrement() {
+        count = 1;
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        IntStream.range(0, NUM_INCREMENTS)
+                .forEach(i -> executor.submit(Synchronized1::increment));
+
+        ConcurrentUtils.stop(executor);
+
+        System.out.println("NonSync: " + count);
+    }
+
+    private static synchronized void incrementSync() {
+        count = count + 1;
+    }
+
+    private static void increment() {
+        count = count + 1;
+    }
+}
