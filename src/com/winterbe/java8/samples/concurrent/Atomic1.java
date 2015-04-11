e98f201d77c00aa6f8e5983dac34fb935fd902f7
Benjamin Winterberg <winterbe@googlemail.com>
2015-04-11 09:11:57 +0200

    Atomic accumulate

--- src/com/winterbe/java8/samples/concurrent/Atomic1.java
+++ src/com/winterbe/java8/samples/concurrent/Atomic1.java
@@ -3,6 +3,7 @@ package com.winterbe.java8.samples.concurrent;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.IntBinaryOperator;
 import java.util.stream.IntStream;
 
 /**
@@ -12,23 +13,43 @@ public class Atomic1 {
 
     private static final int NUM_INCREMENTS = 10000;
 
-    private static AtomicInteger count = new AtomicInteger(0);
+    private static AtomicInteger atomicInt = new AtomicInteger(0);
 
     public static void main(String[] args) {
         testIncrement();
+        testAccumulate();
     }
 
-    private static void testIncrement() {
-        count.set(0);
+    private static void testAccumulate() {
+        atomicInt.set(0);
 
         ExecutorService executor = Executors.newFixedThreadPool(2);
 
         IntStream.range(0, NUM_INCREMENTS)
-                .forEach(i -> executor.submit(count::incrementAndGet));
+                .forEach(i -> {
+                    Runnable task = () -> {
+                        IntBinaryOperator operator = (a, b) -> a + b;
+                        atomicInt.accumulateAndGet(i, operator);
+                    };
+                    executor.submit(task);
+                });
 
         ConcurrentUtils.stop(executor);
 
-        System.out.format("Expected=%d; Is=%d", NUM_INCREMENTS, count.get());
+        System.out.format("Accumulate: %d\n", atomicInt.get());
+    }
+
+    private static void testIncrement() {
+        atomicInt.set(0);
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        IntStream.range(0, NUM_INCREMENTS)
+                .forEach(i -> executor.submit(atomicInt::incrementAndGet));
+
+        ConcurrentUtils.stop(executor);
+
+        System.out.format("Increment: Expected=%d; Is=%d\n", NUM_INCREMENTS, atomicInt.get());
     }
 
 }
