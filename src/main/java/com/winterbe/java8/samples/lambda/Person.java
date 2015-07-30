416e5bf296eb1b45df9cb23bb3f0e8e9696eb98e
pdvlp <pdvlp@qq.com>
2015-07-31 02:47:49 +0800

    add tostring

--- src/main/java/com/winterbe/java8/samples/lambda/Person.java
+++ src/main/java/com/winterbe/java8/samples/lambda/Person.java
@@ -1,16 +1,29 @@
 package com.winterbe.java8.samples.lambda;
 
 /**
-* @author Benjamin Winterberg
-*/
+ * @author Benjamin Winterberg
+ */
 public class Person {
-    public String firstName;
-    public String lastName;
+	public String firstName;
+	public String lastName;
 
-    public Person() {}
+	public Person() {
+	}
 
-    public Person(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+	public Person(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Person [firstName=");
+		builder.append(firstName);
+		builder.append(", lastName=");
+		builder.append(lastName);
+		builder.append("]");
+		return builder.toString();
+	}
+
 }
