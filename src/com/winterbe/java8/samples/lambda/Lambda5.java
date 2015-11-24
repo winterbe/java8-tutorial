543c92513c88abef2c909a8f42f203308cf703bb
grijesh <grijesh.rajoria@gmail.com>
2015-11-25 00:56:51 +0530

    Example of Pre-Defined functional interfaces

--- src/com/winterbe/java8/samples/lambda/Lambda5.java
+++ src/com/winterbe/java8/samples/lambda/Lambda5.java
@@ -1,7 +1,13 @@
 package com.winterbe.java8.samples.lambda;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.UnaryOperator;
 
 /**
  * Created by grijesh
@@ -12,11 +18,11 @@ public class Lambda5 {
     public static void main(String... args) {
 
         //BiConsumer Example
-        BiConsumer<String,Integer> printKeyAndValue
-                = (key,value) -> System.out.println(key+"-"+value);
+        BiConsumer<String, Integer> printKeyAndValue
+                = (key, value) -> System.out.println(key + "-" + value);
 
-        printKeyAndValue.accept("One",1);
-        printKeyAndValue.accept("Two",2);
+        printKeyAndValue.accept("One", 1);
+        printKeyAndValue.accept("Two", 2);
 
         System.out.println("##################");
 
@@ -26,7 +32,33 @@ public class Lambda5 {
         dummyValues.put("Two", 2);
         dummyValues.put("Three", 3);
 
-        dummyValues.forEach((key,value) -> System.out.println(key+"-"+value));
+        dummyValues.forEach((key, value) -> System.out.println(key + "-" + value));
 
+        //BiFunction Example
+        BiFunction<Integer, Integer, Integer> addition = (a, b) -> a + b;
+        addition.apply(1, 2); //3
+
+        //Unary Operator
+        UnaryOperator<Integer> squareOfElements = (a) -> a * a;
+        List<Integer> dummyList = Arrays.asList(1, 2, 3);
+
+        dummyList
+                .stream()
+                .map(squareOfElements::apply)
+                .forEach(System.out::println); // 1 4 9
+
+        // Binary Operator
+        BinaryOperator<String> appendKeyAndValue = (key, value) -> key + ":" + value;
+
+        HashMap<String, String> dummyMap = new HashMap<>();
+        dummyMap.put("ABC", "123");
+        dummyMap.put("XYZ", "321");
+
+        List<String> appendedKeyValuesPair = new ArrayList<>();
+
+        dummyMap
+                .forEach((key, value) -> appendedKeyValuesPair.add(appendKeyAndValue.apply(key, value)));
+
+        appendedKeyValuesPair.forEach(System.out::println); //ABC:123 XYZ:321
     }
 }
