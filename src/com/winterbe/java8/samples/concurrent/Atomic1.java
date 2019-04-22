a9f22c5f3be4f90e79bcc4fdf24f0c50407b9e97
default <19777147+keukenkastjeXYZ@users.noreply.github.com>
2019-04-22 11:08:03 +0200

    Made atomic_int a constant and applied little refactor

--- src/com/winterbe/java8/samples/concurrent/Atomic1.java
+++ src/com/winterbe/java8/samples/concurrent/Atomic1.java
@@ -12,59 +12,59 @@ public class Atomic1 {
 
     private static final int NUM_INCREMENTS = 1000;
 
-    private static AtomicInteger atomicInt = new AtomicInteger(0);
+    private static final AtomicInteger ATOMIC_INT = new AtomicInteger(0);
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         testIncrement();
         testAccumulate();
         testUpdate();
     }
 
     private static void testUpdate() {
-        atomicInt.set(0);
+        ATOMIC_INT.set(0);
 
-        ExecutorService executor = Executors.newFixedThreadPool(2);
+        final ExecutorService executor = Executors.newFixedThreadPool(2);
 
         IntStream.range(0, NUM_INCREMENTS)
                 .forEach(i -> {
                     Runnable task = () ->
-                            atomicInt.updateAndGet(n -> n + 2);
+                            ATOMIC_INT.updateAndGet(n -> n + 2);
                     executor.submit(task);
                 });
 
         ConcurrentUtils.stop(executor);
 
-        System.out.format("Update: %d\n", atomicInt.get());
+        System.out.format("Update: %d\n", ATOMIC_INT.get());
     }
 
     private static void testAccumulate() {
-        atomicInt.set(0);
+        ATOMIC_INT.set(0);
 
-        ExecutorService executor = Executors.newFixedThreadPool(2);
+        final ExecutorService executor = Executors.newFixedThreadPool(2);
 
         IntStream.range(0, NUM_INCREMENTS)
                 .forEach(i -> {
                     Runnable task = () ->
-                            atomicInt.accumulateAndGet(i, (n, m) -> n + m);
+                            ATOMIC_INT.accumulateAndGet(i, Integer::sum);
                     executor.submit(task);
                 });
 
         ConcurrentUtils.stop(executor);
 
-        System.out.format("Accumulate: %d\n", atomicInt.get());
+        System.out.format("Accumulate: %d\n", ATOMIC_INT.get());
     }
 
     private static void testIncrement() {
-        atomicInt.set(0);
+        ATOMIC_INT.set(0);
 
-        ExecutorService executor = Executors.newFixedThreadPool(2);
+        final ExecutorService executor = Executors.newFixedThreadPool(2);
 
         IntStream.range(0, NUM_INCREMENTS)
-                .forEach(i -> executor.submit(atomicInt::incrementAndGet));
+                .forEach(i -> executor.submit(ATOMIC_INT::incrementAndGet));
 
         ConcurrentUtils.stop(executor);
 
-        System.out.format("Increment: Expected=%d; Is=%d\n", NUM_INCREMENTS, atomicInt.get());
+        System.out.format("Increment: Expected=%d; Is=%d\n", NUM_INCREMENTS, ATOMIC_INT.get());
     }
 
 }
