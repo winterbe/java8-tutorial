8c69cdd3cedcd360fa2d17db07a35931910cd93c
thomas43397 <44763395+thomas43397@users.noreply.github.com>
2021-06-11 14:06:08 -0700

    added comments

--- src/com/winterbe/java8/samples/concurrent/Atomic1.java
+++ src/com/winterbe/java8/samples/concurrent/Atomic1.java
@@ -10,7 +10,7 @@ import java.util.stream.IntStream;
  */
 public class Atomic1 {
 
-    private static final int NUM_INCREMENTS = 1000;
+    private static final int NUM_INCREMENTS = 1000; //test
 
     private static AtomicInteger atomicInt = new AtomicInteger(0);
 
